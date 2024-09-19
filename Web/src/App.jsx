3220c9f8239360e4886f33e5346a8d1dc2d6f51c
Ale <alejaman04@gmail.com>
2024-09-19 19:59:14 -0400

    Revert "Removed SideBar and NavBar when on pages involving authentication"
    This reverts commit a25ca28e78c3e6386859a01acaa1f587e3422b76.

--- Web/src/App.jsx
+++ Web/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import { ChakraProvider, Box, Flex } from '@chakra-ui/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import theme from './theme';
@@ -8,52 +8,29 @@ import Sidebar from './components/Sidebar';
 import RoutesWrapper from './components/RoutesWrapper'; // Import the new component
 
 function App() {
-
-  const [isAuthPath,setIsAuthPath] = useState(false)
-
-  // Effect to check the current path and update isAuthPath accordingly
-  useEffect(() => {
-    const authPaths = ['/login', '/signup', '/forgot-password', '/reset-password'];
-    setIsAuthPath(authPaths.includes(location.pathname));
-  }, [location.pathname]);
-
   return (
     <ChakraProvider theme={theme}>
       <Router>
-        {isAuthPath ? (
-            <Box minH="100vh" display="flex" alignItems="center" justifyContent="center">
-              <Flex>
-                <Box
-                    flex="1"
-                    p={8}
-                    overflowY="auto"
-                  >
-                    <RoutesWrapper setIsAuthPath={setIsAuthPath}/> {/* Use the new components */}
-                  </Box>
-              </Flex>
+        <Box minH="100vh">
+          <Navbar />
+          <Flex>
+            <Sidebar />
+            <Box
+              flex="1"
+              ml={{ base: 0, md: '240px' }}
+              mt="60px"
+              p={8}
+              minH="calc(100vh - 60px)"
+              overflowY="auto"
+              width="calc(100% - 240px)"
+            >
+              <RoutesWrapper /> {/* Use the new components */}
             </Box>
-          ) : (
-            <Box minH="100vh">
-              <Navbar />
-              <Flex>
-                <Sidebar />
-                <Box
-                  flex="1"
-                  ml={{ base: 0, md: '240px' }}
-                  mt="60px"
-                  p={8}
-                  minH="calc(100vh - 60px)"
-                  overflowY="auto"
-                  width="calc(100% - 240px)"
-                >
-                  <RoutesWrapper setIsAuthPath={setIsAuthPath}/> {/* Use the new components */}
-                </Box>
-              </Flex>
-            </Box>
-          )}
+          </Flex>
+        </Box>
       </Router>
     </ChakraProvider>
   );
 }
 
-export default App; 
+export default App;
