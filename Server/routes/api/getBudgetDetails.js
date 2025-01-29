b2e90b991c906cc1f22b3df5fc3e0ad717552131
Ale <alejaman04@gmail.com>
2025-01-28 22:15:39 -0500

    created endpoint for getting budget details

---
+++ Server/routes/api/getBudgetDetails.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const router = express.Router();
+const { getSupabaseClientWithAuth } = require('../../services/supabase')
+
+router.post('/', function (req, response, next) {
+    Promise.resolve()
+        .then(async function() {
+            const { sbAccessToken, userID } = req.body;
+
+            if (!sbAccessToken || !userID) {
+                return res.status(400).json({ message: "Missing sbAccessToken or userID" });
+            }
+
+            const supabase = getSupabaseClientWithAuth(sbAccessToken);
+
+            const { data: selectUserBudgetDetails, error: selectUserBudgetDetailsError } = await supabase
+                .from("userBudgets")
+                .select("category, budget")
+                .eq("id", userID);
+
+            if (selectUserBudgetDetailsError){
+                return response.status(400).json({ message: `this error occured trying to get the users budget details: ${selectUserBudgetDetailsError}` })
+            };
+
+            if (selectUserBudgetDetails && selectUserBudgetDetails.length > 0){
+                return response.status(200).json(selectUserBudgetDetails);
+            }else{
+                return response.status(404).json(null)
+            };
+
+        })
+        .catch(next);
+});
+
+module.exports = router;
