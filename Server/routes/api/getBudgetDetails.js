d1bf2d8fd5914886d3d43a3d7017b8fde838bf1a
Ale <alejaman04@gmail.com>
2025-02-04 23:24:40 -0500

    converted response from supabase into category to budget dictionary before sending back to frontend

--- Server/routes/api/getBudgetDetails.js
+++ Server/routes/api/getBudgetDetails.js
@@ -13,6 +13,15 @@ router.post('/', function (req, response, next) {
 
             const supabase = getSupabaseClientWithAuth(sbAccessToken);
 
+            const { data: selectUserBudgetTotal, error: selectUserBudgetTotalError } = await supabase
+                .from("userBudgetTotals")
+                .select("budget_total")
+                .eq("id", userID);
+            
+            if (selectUserBudgetTotalError){
+                return response.status(400).json({ message: `Error occurred trying to get the users total budget: ${selectUserBudgetTotalError}` });
+            }
+
             const { data: selectUserBudgetDetails, error: selectUserBudgetDetailsError } = await supabase
                 .from("userBudgets")
                 .select("category, budget")
@@ -23,7 +32,11 @@ router.post('/', function (req, response, next) {
             };
 
             if (selectUserBudgetDetails && selectUserBudgetDetails.length > 0){
-                return response.status(200).json({ budgetDetails: selectUserBudgetDetails });
+                const budgetObject = selectUserBudgetDetails.reduce((acc, item) => ({
+                    ...acc,
+                    [item.category]: item.budget
+                }), {});
+                return response.status(200).json({ budgetDetails: budgetObject, budgetTotal: selectUserBudgetTotal?.length > 0 ? selectUserBudgetTotal[0].budget_total : 0 });
             }else{
                 return response.status(404).json({ message: 'Could not find budget details for user' })
             };
